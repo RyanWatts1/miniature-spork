c4552e007cb811108b2d58de2d68e799e3bbcdeb
RyanWatts1 <devshop202@yahoo.com>
2020-09-16 11:02:27 -0400

    buttons added to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,10 @@ class NavBar extends React.Component {
     render() {
         return (
             <div className={ styles.navbar }>
-
+                <p>some text</p>
+                <button>Button1</button>
+                <button>Button2</button>
+                <button>Button3</button>
             </div>
         );
     }
