97c6752023802a598568bd6a7c448b758b8ae4f7
RyanWatts1 <devshop202@yahoo.com>
2020-09-16 11:41:00 -0400

    added exact paths for route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import NavBar from './components/NavBar';
+import Home from './Pages/Home';
+import LatestNews from './Pages/LatestNews';
 
 function App() {
   return (
-    <NavBar/>
+    <div>
+      <NavBar/>
+     
+
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/LatestNews" component={ LatestNews } />
+        </Switch>
+      </Router>
+    </div>
+    
   );
 }
 
---
+++ src/Pages/LatestNews.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function LatestNews() {
+    return(
+        <p>This is the current news page!</p>
+    );
+}
+export default LatestNews;
