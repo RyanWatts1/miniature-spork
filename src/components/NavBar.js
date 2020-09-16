f06b296d7e426f72bf1ca6e57b76c85748409c58
RyanWatts1 <devshop202@yahoo.com>
2020-09-16 10:53:50 -0400

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from '../cssModules/NavBar.module.css';
+
+class NavBar extends React.Component {
+    render() {
+        return (
+            <div className={ styles.navbar }>
+
+            </div>
+        );
+    }
+}
+
+export default NavBar;
